e9d9857736e0eeeb3c52b3098f9304dd6ce7d551
Govind Singh <govind.s9551@gmail.com>
2024-06-02 19:50:17 +0530

    Code is here

--- src/Set_matrix_to_zero_method_3.java
+++ src/Set_matrix_to_zero_method_3.java
@@ -1,8 +1,17 @@
+import java.util.*;
 public class Set_matrix_to_zero_method_3{
     public static void main(String[] args){
-        int[][] arr={{0,1,2,0},{3,4,5,2},{1,3,1,5}};
-        int row=arr.length;
-        int column=arr[0].length;
+        Scanner Sc=new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int row=Sc.nextInt();
+        System.out.print("Enter number of columns: ");
+        int column=Sc.nextInt();
+        int[][] arr=new int[row][column];
+        System.out.println("Enter your Matrix: ");
+        for(int i=0;i<row;i++){
+            for(int j=0;j<column;j++){
+                arr[i][j]=Sc.nextInt();
+            }}
         boolean rowZero=false;
         boolean columnZero=true;
         for(int i=0;i<row;i++)if(arr[i][0]==0)rowZero=true;
